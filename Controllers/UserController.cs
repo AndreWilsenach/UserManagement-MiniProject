7b89fb44b3f6bb4796df795f24eee146cd163479
andre.wilsenach <andre.wilsenach@toppanfacetech.com>
2023-08-07 09:23:53 +0200

    Added error handeling on controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -49,25 +49,33 @@ public class UserController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<User>> CreateUser(User user)
     {
+        try { 
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+        catch (Exception error) { 
+            Console.WriteLine(error.Message);
+
+            return BadRequest(error);
+        }
     }
 
     // PUT: api/User/5
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateUser(int id, User user)
     {
-        if (id != user.Id)
+        try
+        {
+            if (id != user.Id)
         {
             return BadRequest();
         }
 
         _context.Entry(user).State = EntityState.Modified;
 
-        try
-        {
+     
             await _context.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException)
@@ -89,6 +97,7 @@ public class UserController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteUser(int id)
     {
+        try { 
         var user = await _context.Users.FindAsync(id);
         if (user == null)
         {
@@ -96,13 +105,23 @@ public class UserController : ControllerBase
         }
 
         _context.Users.Remove(user);
+        
         await _context.SaveChangesAsync();
 
         return NoContent();
+        }
+        catch (Exception error) {
+        return BadRequest(error.Message);
+        }
     }
 
     private bool UserExists(int id)
     {
+        try {
         return _context.Users.Any(e => e.Id == id);
+        }
+        catch (Exception error) {
+            return false;
+                }
     }
 }
